9785bf0631f6429391c51e8be5a99a7ba8282d91
braulioportela79 <braulioportela@outlook.com>
2022-05-01 11:37:07 -0300

    Changing Data from Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
+import { useDispatch } from 'react-redux';
+import { setName, setAge } from './redux/reducers/userReducer';
+
 import { useAppSelector } from './redux/hooks/useAppSelector';
+import React from 'react';
+
 function App() {
+  const dispatch = useDispatch();
   const user = useAppSelector((state) => state.user);
+
+  const changeName = (newName: string) => dispatch(setName(newName));
+  const changeAge = (newAge: number) => dispatch(setAge(newAge));
+
+  const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeName(e.target.value);
+  };
+
+  const handleAgeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeAge(parseInt(e.target.value));
+  };
 
   return (
     <div>
       My name is: {user.name} and I am {user.age} years old. <br />
       Theme: ...
       <br />
-      <input type='text' value={user.name} />
+      <input type='text' value={user.name} onChange={handleNameInput} />
+      <input type='text' value={user.age} onChange={handleAgeInput} />
       <hr />
       <button>Switch Theme</button>
     </div>
