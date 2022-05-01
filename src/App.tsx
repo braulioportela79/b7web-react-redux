7f4d689144350da83d8af3c6f7b80c9495de99a8
braulioportela79 <braulioportela@outlook.com>
2022-05-01 11:26:44 -0300

    Display Reducer Data and Creating Redux Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useAppSelector } from './redux/hooks/useAppSelector';
 function App() {
+  const user = useAppSelector((state) => state.user);
+
   return (
     <div>
-      My name is: .... and I am ... years old. <br />
+      My name is: {user.name} and I am {user.age} years old. <br />
       Theme: ...
       <br />
-      <input type='text' value={'...'} />
+      <input type='text' value={user.name} />
       <hr />
       <button>Switch Theme</button>
     </div>
